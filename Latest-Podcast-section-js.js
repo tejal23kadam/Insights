e8e76bd234e300b67c65cf419c0c2b0995fd49b4
tejal23kadam <154776489+tejal23kadam@users.noreply.github.com>
2024-03-30 12:57:30 +0530

    changes in home file

--- Latest-Podcast-section-js.js
+++ Latest-Podcast-section-js.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
  
   $("#news-slider").owlCarousel({
-          items: 3,
+          items: 1,
           margin:20,
           itemsDesktop: [1199, 3],
           itemsDesktopSmall: [980, 2],
           itemsMobile: [600, 1],
           navigation: true,
           loop:true,
-    
+     
           navigationText: ["", ""],
           //pagination: false, //for version 1
           dots: false, // for version 2         
@@ -16,7 +16,7 @@ $(document).ready(function() {
           //autoplay: true,//for version 2 
           slideTransition: 'linear',
           
-          autoplaySpeed: 1000,
+          autoplaySpeed: 1000,  
         
           
           responsiveClass:true,
