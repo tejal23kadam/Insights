7dd5373471a2696a0c03861afe4003999839353d
tejal23kadam <154776489+tejal23kadam@users.noreply.github.com>
2024-04-25 13:15:13 +0530

    code added

--- Latest-Review-section-js.js
+++ Latest-Review-section-js.js
@@ -14,7 +14,29 @@ $(document).ready(function() {
             dots: true, // for version 2         
            
           });
-    });
+          var owl = $(".owl-carousel");
+          owl.owlCarousel();
+          $(".next-btn").click(function () {
+            owl.trigger("next.owl.carousel");
+          });
+          $(".prev-btn").click(function () {
+            owl.trigger("prev.owl.carousel");
+          });
+          $(".prev-btn").addClass("disabled");
+          $(owl).on("translated.owl.carousel", function (event) {
+            if ($(".owl-prev").hasClass("disabled")) {
+              $(".prev-btn").addClass("disabled");
+            } else {
+              $(".prev-btn").removeClass("disabled");
+            }
+            if ($(".owl-next").hasClass("disabled")) {
+              $(".next-btn").addClass("disabled");
+            } else {
+              $(".next-btn").removeClass("disabled");
+            }
+          });
+        });
+
    
   
     $(document).ready(function(){
