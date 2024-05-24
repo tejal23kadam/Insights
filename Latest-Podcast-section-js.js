b0e8eb28775f3aa4c100cce490e48f3374be78d3
tejal23kadam <154776489+tejal23kadam@users.noreply.github.com>
2024-05-24 13:07:54 +0530

    new code added

--- Latest-Podcast-section-js.js
+++ Latest-Podcast-section-js.js
@@ -12,7 +12,7 @@ $(document).ready(function () {
         //pagination: false, //for version 1
         dots: false, // for version 2         
         //autoPlay:true // for version 1 
-        autoplay: true,//for version 2 
+        //autoplay: true,//for version 2 
         slideTransition: 'linear',
         autoplaySpeed: 1000,
         responsiveClass: true,
@@ -39,15 +39,53 @@ $(document).ready(function () {
                 items: 3,
                 nav: true,
                 loop: true
-            },
-            1550: {
-                items: 3,
-                nav: true,
-                loop: true
             }
         }
     });
 
+    $("#video-news-slider").owlCarousel({
+        items: 1,
+        margin: 20,
+        itemsDesktop: [1199, 3],
+        itemsDesktopSmall: [980, 2],
+        itemsMobile: [600, 1],
+        navigation: false,
+        loop: true,
+
+        //pagination: false, //for version 1
+        dots: false, // for version 2         
+        //autoPlay:true // for version 1 
+        //autoplay: true,//for version 2 
+        slideTransition: 'linear',
+        autoplaySpeed: 1000,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: true
+            },
+            600: {
+                items: 1,
+                nav: false
+            },
+            800: {
+                items: 2,
+                nav: true,
+                loop: true
+            },
+            1000: {
+                items: 2,
+                nav: true,
+                loop: true
+            },
+            1200: {
+                items: 2,
+                nav: true,
+                loop: true
+            }
+
+        }
+    });
 
     $("#hindi-slider").owlCarousel({
         items: 1,
@@ -88,12 +126,8 @@ $(document).ready(function () {
                 items: 3,
                 nav: true,
                 loop: true
-            },
-            1550: {
-                items: 3,
-                nav: true,
-                loop: true
             }
+
         }
     });
 
@@ -136,12 +170,8 @@ $(document).ready(function () {
                 items: 3,
                 nav: true,
                 loop: true
-            },
-            1550: {
-                items: 3,
-                nav: true,
-                loop: true
             }
+
         }
     });
 
@@ -153,6 +183,13 @@ $(document).ready(function () {
         $("#news-slider").trigger('prev.owl.carousel');
     });
 
+    // video page >  latest podcast >  Custom Button
+    $('#customNextBtn1').click(function () {
+        $("#video-news-slider").trigger('next.owl.carousel');
+    });
+    $('#customPreviousBtn2').click(function () {
+        $("#video-news-slider").trigger('prev.owl.carousel');
+    });
 
     // Hindi Custom Button
     $('#HindicustomNextBtn1').click(function () {
